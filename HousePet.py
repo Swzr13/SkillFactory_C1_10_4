edff7ee1f85b9e3c8a05ae39c94314aeb17dc29a
Swzr13 <79765626+Swzr13@users.noreply.github.com>
2021-03-13 19:05:32 +0300

    Add files via upload

---
+++ HousePet.py
@@ -0,0 +1,8 @@
+from ClassHousePet import Guest
+
+guests = [Guest(name='Vasin', adress='Moskow', status='Mentor'),\
+          Guest(name='Stanovoy', adress='Tomsk', status='Friend'),\
+          Guest(name='Nastepanin', adress='Moskow', status='Mentor')]
+
+for elem in guests:
+    print(elem)
